b84428c43fd6e3d134da0c1eafe16318f5b16253
moonseunghun <mangokkaksae2@gmail.com>
2023-09-28 15:56:38 +0900

    ADD : 데이터 모델 래핑

---
+++ lib/model/model.dart
@@ -0,0 +1,21 @@
+import 'dart:ffi';
+
+class Restaurant {
+  final String id;
+  final String thumbUrl;
+  final List<String> tags;
+  final Int number ;
+
+  RestaurantModel ({
+
+    required this.id,
+    required this.thumbUrl,
+    required this.tags,
+    required this.number;
+
+
+   })
+
+}
+
+
