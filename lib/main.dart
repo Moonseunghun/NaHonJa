c1a1fda0f6346e0c16d1c4f34857ef3b3d628bbe
moonseunghun <mangokkaksae2@gmail.com>
2023-09-28 18:08:55 +0900

    Chore : class 구조 화

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'BottomNavBar',
-      theme: ThemeData(
-        primarySwatch: Colors.grey,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _currentIndex = 0;
-
-  // final List<Widget> _pages = [MyAppHome(), CalendarApp(), CombinedApp()];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('AwerTImes'),
-      ),
-      // body: _pages[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (int index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'ChartApp',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_month),
-            label: 'CalendarApp',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'TotalApp',
-          ),
-        ],
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello, Flutter!'),
+        ),
+        body: Center(
+          child: Text('Hello, Flutter!'),
+        ),
       ),
     );
   }
 }
+
 
 
--- lib/model/model.dart
+++ lib/model/model.dart
@@ -1,21 +1,27 @@
-import 'dart:ffi';
+// import 'dart:ffi';
+//
+// class Restaurant {
+//   final String id;
+//   final String thumbUrl;
+//   final List<String> tags;
+//   final Int number;
+//
+//   RestaurantModel({
+//
+//     required this.id,
+//     required this.thumbUrl,
+//     required this.tags,
+//     required this.number;
+//
+//
+//   });
+//
+//   factory RestaurantModel.fromjson ({
+//     required Map<String , dynamic> json,
+// }) {
+//   return RestaurantModel(id: id, thumbUrl: thumbUrl, tags: tags, number: number)
+//   }
+//
+// }
 
-class Restaurant {
-  final String id;
-  final String thumbUrl;
-  final List<String> tags;
-  final Int number ;
-
-  RestaurantModel ({
-
-    required this.id,
-    required this.thumbUrl,
-    required this.tags,
-    required this.number;
-
-
-   })
-
-}
-
-
+  //Factory 예제
